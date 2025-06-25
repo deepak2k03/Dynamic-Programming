f3ebab8bdd828637e2544da5944bd7a372a1470c
Deepak Singh <137672814+deepak2k03@users.noreply.github.com>
2025-06-25 11:37:53 +0530

    Create 43_Unbounded_Knapsack_Tabulation.cpp

---
+++ 43_Unbounded_Knapsack_Tabulation.cpp
@@ -0,0 +1,20 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    int knapSack(vector<int>& val, vector<int>& wt, int W) {
+        // code here
+        vector<vector<int>>dp(val.size(),vector<int>(W+1,0));
+        for(int w=0;w<=W;w++) if (wt[0] <= w) dp[0][w] = val[0] + dp[0][w-wt[0]];
+        for(int i=1;i<val.size();i++){
+            for(int j=0;j<=W;j++){
+                int nt=dp[i-1][j];
+                int t=INT_MIN;
+                if(wt[i]<=j) t=val[i]+dp[i][j-wt[i]];
+                dp[i][j]=max(t,nt);
+            }
+        }
+        return dp[val.size()-1][W];
+        
+    }
+};
