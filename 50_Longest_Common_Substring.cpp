b539c9717c1f089aa6c2e4923258a993bc0d5a46
Deepak Singh <137672814+deepak2k03@users.noreply.github.com>
2025-06-26 08:56:30 +0530

    Create 50_Longest_Common_Substring.cpp

---
+++ 50_Longest_Common_Substring.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int longestCommonSubstr(string& text1, string &text2) {
+        // your code here
+        int n = text1.size(), m = text2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for (int i=0;i<=n;i++) dp[i][0]=0;
+        for (int j=0;j<=m;j++) dp[0][j]=0;
+
+        for (int i=1;i<=n;i++) {
+            for (int j=1;j<=m;j++) {
+                if(text1[i-1]==text2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=0;
+            }
+        }
+        int mx=0;
+        for(int i=0;i<=n;i++) for(int j=0;j<=m;j++) mx=max(mx,dp[i][j]);
+        return mx;
+    }
+};
