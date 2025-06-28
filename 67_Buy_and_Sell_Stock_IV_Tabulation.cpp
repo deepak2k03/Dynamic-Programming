3b07909e0b72898e2344a02dae79ef55eb0b16b3
Deepak Singh <137672814+deepak2k03@users.noreply.github.com>
2025-06-28 12:19:38 +0530

    Create 67_Buy_and_Sell_Stock_IV_Tabulation.cpp

---
+++ 67_Buy_and_Sell_Stock_IV_Tabulation.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(k+1,0)));
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                for(int cap=1;cap<=k;cap++){
+                    int op1=0,op2=0;
+                    if(buy){
+                        op1=-prices[ind]+dp[ind+1][0][cap];
+                        op2=dp[ind+1][1][cap];
+                    }
+                    else{
+                        op1=prices[ind]+dp[ind+1][1][cap-1];
+                        op2=dp[ind+1][0][cap];
+                    }
+                    dp[ind][buy][cap]=max(op1,op2);
+                }
+            }
+        } 
+        return dp[0][1][k];
+    }
+};
